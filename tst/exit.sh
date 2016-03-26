ef3466e9d60ed6cdae5d63aa277abce7d398ecb8
René de Hesselle <dehesselle@web.de>
2016-03-26 20:31:37 +0100

    Add function to quit if being sourced

---
+++ tst/exit.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#BL_SRC=$(dirname $0)/../src
+
+BL_SRC=/Users/rene/Developer/bashlib/src
+
+. $BL_SRC/bl_dep
+source $BL_SRC/bl_exit
+
+bl_exit_if_sourced
+
+echo "Hello World."
+
